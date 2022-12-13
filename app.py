0b6db506e6a1ac9cf30a6446b79fe75f1eb082be
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-13 19:28:14 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+pip install streamlit
+
+# you will be able to see the title with the given content
+st.title('Welcome to Evoke Technologies')
+
+st.header('This is a header')  # same goes with st.subheader('xxxxxxx')
+
+st.text('This is used to display simple text')
+
+st.success('This is used to display Success messages in a green box')
+
+st.warning('This is used to display Warning message in orange box')
+
+st.error('This is used to display Error messages in red box')
+
+st.info('This is used to display Information in a blue box')
